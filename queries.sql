267291b986277f556e07f967d8c81b2f6a987bf6
Adam Cooper <mail@admcpr.com>
2023-03-13 14:48:31 +0100

    Create

---
+++ queries.sql
@@ -0,0 +1,103 @@
+/* Naive title queries */
+select title from assets where title like '%star wars%' -- :( No results
+
+select title from assets where title like '%star%' or title like '%wars%' -- :( More results, but still not Star Wars ??
+
+-- Oh no like is case sensitive
+select title from assets where title ilike '%star%' or title ilike '%wars%' -- :( Even more results, and Star Wars but where is it?
+
+-- Help me tsvector you are my only hope
+select to_tsvector('I Am Altering The Deal. Pray I Don''t Alter It Any Further!');
+
+-- Let's do that to some of our titles
+select to_tsvector(title), title from assets limit 20;
+
+-- So maybe we can query that with to_tsquery
+select title 
+from assets
+where to_tsvector(title) @@ to_tsquery('Star') -- :| OK
+
+-- And can we rank the results perhaps?
+select title, ts_rank(to_tsvector(title), to_tsquery('Star')) as rank
+from assets
+where to_tsvector(title) @@ to_tsquery('star') -- :/ Interesting
+
+-- So let's find star wars 🎉
+select title, ts_rank(to_tsvector(title), to_tsquery('star wars')) as rank
+from assets
+where to_tsvector(title) @@ to_tsquery('star wars') -- :( Oh come on!
+
+-- Better error messages would be nice but hey ...
+select title, ts_rank(to_tsvector(title), plainto_tsquery('star wars')) as rank
+from assets
+where to_tsvector(title) @@ plainto_tsquery('star wars')
+order by rank desc -- :) OK that's cool
+
+-- I want to search like google though
+select title, ts_rank(to_tsvector(title), websearch_to_tsquery('"star wars" -lego')) as rank
+from assets
+where to_tsvector(title) @@ websearch_to_tsquery('"star wars" -lego')
+order by rank desc -- :) No more lego
+
+-- What if you love lego ...
+select title, ts_rank(to_tsvector(title), websearch_to_tsquery('"star wars" +lego')) as rank
+from assets
+where to_tsvector(title) @@ websearch_to_tsquery('"star wars" +lego')
+order by rank desc -- :) Only lego
+
+-- What if I need to query multiple columns? 
+select to_tsvector(title), to_tsvector("AKATitle")
+from assets
+where "AKATitle" is not null -- :( I don't want to query them separately though
+
+-- Let's concatenate them 
+select to_tsvector(title) || ' ' || to_tsvector("AKATitle")
+from assets
+where "AKATitle" is not null -- :| Oops, just remembered I need to weight them
+
+-- Set weights
+select setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector("AKATitle"), 'B')
+from assets
+where "AKATitle" is not null -- :\ Better, but how am I going to query this?
+
+-- Well we could store the weighted vectors in a column
+--
+-- alter table assets add title_search tsvector;
+-- update assets set title_search = 
+-- 	setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector("AKATitle"), 'B')
+--
+-- We're not going to though because running this would take minutes and how do we keep the column
+-- up to date ... with triggers? Yuck ... 🤢
+
+-- Postgres is, once again, going to show how awesome it is now
+-- alter table assets drop title_search;
+alter table assets 
+add title_search tsvector 
+generated always as	(
+	setweight(to_tsvector('simple', coalesce(title, '')), 'A') || ' ' || 
+	setweight(to_tsvector('simple', coalesce("AKATitle", '')), 'B') :: tsvector
+) stored; -- What's going on with 'simple' and coalesce????? - Avoids immutable errors
+
+-- Does that mean I've got a new search column I can query that's always up to date
+select title, "AKATitle"
+from assets 
+where title_search @@ websearch_to_tsquery('"star wars" +lego')
+-- :) Why yes it does, if only we could make this fast 
+
+-- Like with some kind of special index optimised for vectors
+create index idx_search on assets using GIN(title_search)
+
+-- Is this fast enough?
+-- Remember we're querying multiple columns with full text search, weighting and web search features
+select title, "AKATitle" 
+from assets 
+where title_search @@ websearch_to_tsquery('"star wars" +lego'); -- :)))))))
+
+-- And now let's rank based on the length of the document (normalization yay https://www.postgresql.org/docs/current/textsearch-controls.html#TEXTSEARCH-RANKING)
+select title, ts_rank(title_search, plainto_tsquery('friends'), 2) as rank, collection_type
+from collections
+where title_search @@ plainto_tsquery('friends')
+and collection_type = 'series'
+order by rank desc
+
+-- THE END
