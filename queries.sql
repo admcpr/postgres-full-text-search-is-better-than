1cac372af7f371aec22953924b9e0702e0b6cad1
Adam Cooper <mail@admcpr.com>
2023-04-23 20:54:48 +0200

    Update queries

--- queries.sql
+++ queries.sql
@@ -39,34 +39,31 @@ from movies
 where to_tsvector(title) @@ websearch_to_tsquery('"star wars" -clone')
 order by rank desc -- :) No more clone wars
 
--- What if you love lego ...
+-- What if you love clone wars ...
 select title, ts_rank(to_tsvector(title), websearch_to_tsquery('"star wars" +clone')) as rank
 from movies
 where to_tsvector(title) @@ websearch_to_tsquery('"star wars" +clone')
 order by rank desc -- :) Only clone wars
 
 -- What if I need to query multiple columns? 
-select to_tsvector(title), to_tsvector("AKATitle")
-from movies
-where "AKATitle" is not null -- :( I don't want to query them separately though
+select to_tsvector(title), to_tsvector(original_title)
+from movies -- :( I don't want to query them separately though
 
 -- Let's concatenate them 
-select to_tsvector(title) || ' ' || to_tsvector("AKATitle")
-from movies
-where "AKATitle" is not null -- :| Oops, just remembered I need to weight them
+select to_tsvector(title) || ' ' || to_tsvector(original_title)
+from movies -- :| Oops, just remembered I need to weight them
 
 -- Set weights
-select setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector("AKATitle"), 'B')
-from movies
-where "AKATitle" is not null -- :\ Better, but how am I going to query this?
+select setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector(original_title), 'B')
+from movies -- :\ Better, but how am I going to query this?
 
 -- Well we could store the weighted vectors in a column
 --
 -- alter table movies add title_search tsvector;
 -- update movies set title_search = 
--- 	setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector("AKATitle"), 'B')
+-- 	setweight(to_tsvector(title), 'A') || ' ' || setweight(to_tsvector(original_title), 'B')
 --
--- We're not going to though because running this would take minutes and how do we keep the column
+-- We're not going to though because running this would take too long and how do we keep the column
 -- up to date ... with triggers? Yuck ... 🤢
 
 -- Postgres is, once again, going to show how awesome it is now
@@ -75,13 +72,14 @@ alter table movies
 add title_search tsvector 
 generated always as	(
 	setweight(to_tsvector('simple', coalesce(title, '')), 'A') || ' ' || 
-	setweight(to_tsvector('simple', coalesce("AKATitle", '')), 'B') :: tsvector
-) stored; -- What's going on with 'simple' and coalesce????? - Avoids immutable errors
+	setweight(to_tsvector('simple', coalesce(original_title, '')), 'B') :: tsvector
+) stored; 
 
 -- Does that mean I've got a new search column I can query that's always up to date
-select title, "AKATitle"
-from movies 
-where title_search @@ websearch_to_tsquery('"star wars" +lego')
+select title, original_title, ts_rank(title_search, websearch_to_tsquery('hero')) as rank
+from movies
+where title_search @@ websearch_to_tsquery('hero')
+order by rank desc
 -- :) Why yes it does, if only we could make this fast 
 
 -- Like with some kind of special index optimised for vectors
@@ -89,15 +87,15 @@ create index idx_search on movies using GIN(title_search)
 
 -- Is this fast enough?
 -- Remember we're querying multiple columns with full text search, weighting and web search features
-select title, "AKATitle" 
-from movies 
-where title_search @@ websearch_to_tsquery('"star wars" +lego'); -- :)))))))
+select title, ts_rank(title_search, websearch_to_tsquery('rush')) as rank
+from movies
+where title_search @@ websearch_to_tsquery('rush')
+order by rank desc -- :)))))))
 
 -- And now let's rank based on the length of the document (normalization yay https://www.postgresql.org/docs/current/textsearch-controls.html#TEXTSEARCH-RANKING)
-select title, ts_rank(title_search, plainto_tsquery('friends'), 2) as rank, collection_type
-from collections
-where title_search @@ plainto_tsquery('friends')
-and collection_type = 'series'
+select title, ts_rank(title_search, websearch_to_tsquery('rush'), 1) as rank
+from movies
+where title_search @@ websearch_to_tsquery('rush')
 order by rank desc
 
 -- THE END
